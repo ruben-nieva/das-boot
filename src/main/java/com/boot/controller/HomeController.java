000778dd4d943551c39ef56153fd6cd277c265d8
Ruben Nieva <ruben.nieva@globant.com>
2017-02-19 11:16:40 -0300

    Initial commit

---
+++ src/main/java/com/boot/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.boot.controller;
+
+//import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String home() {
+		return "Das Boot, reporting for duty!";
+	}
+	
+
+}
